8c829b37ebfb0946779e2c40f2243397e9227cf0
Ricardo Pacheco Gonzalez <garnan92@gmail.com>
2022-07-17 22:01:25 -0500

    introduction to shemas and models

--- Api/src/database/conections/index.ts
+++ Api/src/database/conections/index.ts
@@ -3,5 +3,5 @@ import { MONGO_DB } from "interfaces/constants";
 
 export const conectDB = async () => {
   const db = await connect(MONGO_DB);
-  console.log("conexion exitosa a ", db.connection.db.databaseName);
+  console.log("conexion exitosa a", db.connection.db.databaseName);
 };
